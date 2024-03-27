fd9dc427067468ccb7c8f6b0f72a7fda8562fe94
Aniruddha <103273966+aniruddha28715@users.noreply.github.com>
2024-03-27 22:17:26 +0530

    Add files via upload

--- code_1.py
+++ code_1.py
@@ -161,4 +161,95 @@
 # string is immutable while list is mutable
 #mutable means we can change the value of a particular element in a list but cannot do so with a string 
 
-#Tuple
+#Tuple 
+# tuple is similar to list but readonly i.e., once you assign a value to it ,you cannot change that value
+# tup1 = (1,2,3,4,5)
+# print(type(tup1))   
+# # tup1[1]=6          #this will give an error because tuples are read only    
+#                      #we cannot perform any operation which changes the value of elements present inside them
+# print(tup1[1:3])
+# print(tup1.count(2))
+
+
+#practice
+# movies = []
+# mov = input("enter 1st movie: ")
+# movies.append(mov)
+# mov = input("enter 2nd movie: ")
+# movies.append(mov)
+# mov = input("enter 3rd movie: ")
+# movies.append(mov)
+
+#movies.append(input("enter 1st movie: ")) can write like this also
+
+# print(movies)
+
+
+#palidrome means  same word forwards as well as backwards
+# def check_palindrome(word):
+#     return word == word[::-1]   # [::-1] is used to reverse the string
+
+# print(check_palindrome('radar'))
+# print(check_palindrome('python'))   
+
+
+#pratice2
+# list1 = [1,2,1]
+# list2 = [1,2,3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list2):
+#     print("palindrome")
+# else:
+#     print("NOT palindrome")
+
+
+#practice3
+# grade = ["C", " D", "A", "A", "B", "B", "A" ]
+# print(grade.count)
+
+# grade.sort()
+# print(grade)
+
+
+
+#practice4 finding factorial of  a number using recursion
+# def find_factorial(n):
+#     if n==0 or n==1 :
+#         return 1
+#     else:
+#         return n * find_factorial(n-1)
+        
+# print(find_factorial(5))
+
+
+# '''
+# In this code we have two lists named 'list1' and 'list2'. We are trying to check whether 'list1' is equal to 'list2' in reverse order. To
+# Recursion means calling the function within itself in such a way that it reduces the problem size and comes back to the original function again and again
+# In the below code we are trying to create a class for student which will have attributes like name , age and gpa . We also want to
+# The following code will convert all uppercase letters in a given sentence into lowercase and vice versa.
+# '''
+# sentence = 'Hello World'
+# lower_upper = []
+# for char in sentence:
+#     if char.islower():
+#         lower_upper.append(char.upper())
+#     elif char.isupper():
+#         lower_upper.append(char.lower())
+#     else:
+#         lower_upper.append(char)
+
+# result = ''.join(lower_upper)
+# print(result)
+
+
+
+
+
+
+
+
+
+#Dictionary
